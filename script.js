be566f4e23754130a5141046d2ddc2cd92176105
Odunayo <62405700+odunaayo@users.noreply.github.com>
2020-04-12 10:32:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var name= "Obazuaye Odunayo";
+console.log(name);
+
+var course = ["html", "Css", "javascript"];
+  
+console.log (course);
+
+//the number of my course is an odd number
+//if the modulus of the current number divided by 2 is not equal to zero then it should console.log
+for ( let i=0; i <= 200; i++){
+    if (i % 2 !== 0){
+        console.log(i)
+
+    } ; 
+}
